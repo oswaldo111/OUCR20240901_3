6171a41207b1453bebb2bb3c452286ce9ed29bc2
oswaldo chilin <oswaldochilin49@gmail.com>
2024-09-03 14:15:15 -0600

    Agregar archivos de proyecto.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace OUCR20240901_3
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
